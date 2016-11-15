620c7ad677d1393f181e9ec42fa28541e77d30c1
nabadov <nabadov.r@gmail.com>
2016-11-15 15:53:49 +0400

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,78 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modalLogin = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modalLogin.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modalLogin.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+
+  $ionicModal.fromTemplateUrl('templates/profile.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modalProfile = modal;
+  });
+
+  $scope.closeProfile = function() {
+    $scope.modalProfile.hide();
+  };
+
+  // Open the login modal
+  $scope.profile = function() {
+    $scope.modalProfile.show();
+  };
+
+})
+
+.controller('SearchCtrl', function($scope)  {
+  $scope.travelDate = Date.now();
+  $scope.returnDate = 'Return?';
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
